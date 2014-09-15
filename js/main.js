1f132f27582c471ba84938f258ea9a7e7ebbc731
David Landais <david.landais44@gmail.com>
2014-09-15 17:01:36 +0200

    Added .bowerrc and updated headjs loader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 head.load([
-    {"jQuery":              '/vendor/jquery/dist/jquery.js'},
-    {"jQuery-ui":           '/vendor/jquery-ui/jquery-ui.js'},
+    {"jquery":              '/vendor/jquery/dist/jquery.js'},
+    {"jquery-ui":           '/vendor/jquery-ui/jquery-ui.js'},
     {"bootstrap":           '/vendor/bootstrap/dist/js/bootstrap.js'},
     {"jasny-bootstrap":     '/vendor/jasny-bootstrap/dist/js/jasny-bootstrap.js'},
     {"angular":             '/vendor/angular/angular.js'},
@@ -8,14 +8,16 @@ head.load([
     {"angular-bootstrap":   '/vendor/angular-bootstrap/ui-bootstrap.js'},
     {"angular-ui-utils":    '/vendor/angular-ui-utils/ui-utils.js'},
     {"angular-ui-select2":  '/vendor/angular-ui-select2/src/select2.js'},
+    {"angular-animate":     '/vendor/angular-animate/angular-animate.js'},
     {"lodash":              '/vendor/lodash/dist/lodash.js'},
-    {"cssua":               '/vendor/cssua/index.js'}
-]);
+    {"cssua":               '/vendor/cssua/index.js'},
 
-head.load([
     {"normalize":           '/vendor/normalize-css/normalize.css'},
-    {"jQuery-ui":           'vendor/jquery-ui/themes/ui-lightness/jquery-ui.css'},
+    {"jquery-ui":           '/vendor/jquery-ui/themes/ui-lightness/jquery-ui.css'},
     {"bootstrap":           '/vendor/bootstrap/dist/css/bootstrap.css'},
     {"jasny-bootstrap":     '/vendor/jasny-bootstrap/dist/css/jasny-bootstrap.css'},
-    {"select2-bootstrap":   '/vendor/select2/select2-bootstrap.css'}
+    {"select2-bootstrap":   '/vendor/select2/select2-bootstrap.css'},
+    {"animate":             '/vendor/animate/animate.css'},
+
+    {"style":               '/css/style.css'}
 ]);
